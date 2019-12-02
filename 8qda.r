cc01e86b24bdaed3d0e27750da8efe4308c3a949
Zubin Jimmy Paymaster <zubinpaymaster@yahoo.co.in>
2019-12-02 22:56:47 +0530

    Update 8qda.r

--- 8qda.r
+++ 8qda.r
@@ -1,5 +1,3 @@
-# https://datascienceplus.com/how-to-perform-logistic-regression-lda-qda-in-r/
-
 library(MASS)
 library(ggplot2)
 titanicDS = read.csv("titanic.csv")
@@ -31,51 +29,6 @@ train = titanicDS[row.number,]
 test = titanicDS[-row.number,]
 dim(train)
 dim(test)
-
-# logistic regression
-attach(train)
-model1 = glm(factor(Survived)~.-PassengerId-Name-Ticket-Cabin, data=train, family=binomial)
-summary(model1)
-
-#Remove Not significant features.
-model2 = update(model1, ~.-Parch-Fare-Embarked)
-summary(model2)
-
-###Predict for training data and find training accuracy
-pred.prob = predict(model2, type="response")
-pred.prob = ifelse(pred.prob > 0.5, 1, 0)
-table(pred.prob, Survived)
-
-
-##Predict for test Data and find the test accuracy.
-attach(test)
-pred.prob = predict(model2, newdata= test, type="response")
-pred.prob = ifelse(pred.prob > 0.5, 1, 0)
-table(pred.prob, Survived)
-
-
-
-# LDA model
-attach(train)
-lda.model = lda (factor(Survived)~factor(Pclass)+Sex+Age+SibSp, data=train)
-lda.model
-
-
-##Predicting training results.
-predmodel.train.lda = predict(lda.model, data=train)
-table(Predicted=predmodel.train.lda$class, Survived=Survived)
-
-ldahist(predmodel.train.lda$x[,1], g= predmodel.train.lda$class)
-
-
-attach(test)
-predmodel.test.lda = predict(lda.model, newdata=test)
-table(Predicted=predmodel.test.lda$class, Survived=test$Survived)
-
-par(mfrow=c(1,1))
-plot(predmodel.test.lda$x[,1], predmodel.test.lda$class, col=test$Survived+10)
-
-
 
 # QDA model
 
