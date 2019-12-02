67fadf7927a7261094c56f671720892af8cbb40c
Zubin Jimmy Paymaster <zubinpaymaster@yahoo.co.in>
2019-12-02 22:55:49 +0530

    Update 7lda.r

--- 7lda.r
+++ 7lda.r
@@ -1,26 +1,51 @@
-# library(devtools)
-# devtools::install_github("Displayr/flipMultivariates")
+library(MASS)
+library(ggplot2)
+titanicDS = read.csv("titanic.csv")
+dim(titanicDS)
+
+str(titanicDS)
+
+summary(titanicDS)
+
+attach(titanicDS)
+UniqueValue = function (x) {length(unique(x)) }
+apply(titanicDS, 2, UniqueValue)
+
+NaValue = function (x) {sum(is.na(x)) }
+apply(titanicDS, 2, NaValue)
+
+BlankValue = function (x) {sum(x=="") }
+apply(titanicDS, 2, BlankValue)
+
+MissPercentage = function (x) {100 * sum (is.na(x)) / length (x) }
+apply(titanicDS, 2, MissPercentage)
+
+titanicDS$Age[is.na(titanicDS$Age)] = mean(titanicDS$Age, na.rm=TRUE)
+apply(titanicDS, 2, MissPercentage)
+
+set.seed(1)
+row.number = sample(1:nrow(titanicDS), 0.6*nrow(titanicDS))
+train = titanicDS[row.number,]
+test = titanicDS[-row.number,]
+dim(train)
+dim(test)
+
+# LDA model
+attach(train)
+lda.model = lda (factor(Survived)~factor(Pclass)+Sex+Age+SibSp, data=train)
+lda.model
 
 
-# install.packages('rgdal', repos='http://r.findata.org')
-library(flipMultivariates)
+##Predicting training results.
+predmodel.train.lda = predict(lda.model, data=train)
+table(Predicted=predmodel.train.lda$class, Survived=Survived)
 
-vehicles <- read.csv('vehicles.csv')
-lda <- LDA(class ~ ., data = vehicles)
+ldahist(predmodel.train.lda$x[,1], g= predmodel.train.lda$class)
 
-# lda
 
-lda.2 <- LDA(class ~ COMPACTNESS + CIRCULARITY + DISTANCE.CIRCULARITY + RADIUS.RATIO,
-             data = vehicles,
-             output = "Scatterplot",
-             prior = "Equal",
-             subset = vehicles$ELONGATEDNESS < 50,
-             weight = ifelse(vehicles$class == "saab", 2, 1))
-print(lda.2)
+attach(test)
+predmodel.test.lda = predict(lda.model, newdata=test)
+table(Predicted=predmodel.test.lda$class, Survived=test$Survived)
 
-LDA(class ~ COMPACTNESS + CIRCULARITY + DISTANCE.CIRCULARITY + RADIUS.RATIO,
-    data = vehicles,
-    output = "Prediction-Accuracy Table",
-    prior = "Equal",
-    subset = vehicles$ELONGATEDNESS < 50,
-    weight = ifelse(vehicles$class == "saab", 2, 1))
+par(mfrow=c(1,1))
+plot(predmodel.test.lda$x[,1], predmodel.test.lda$class, col=test$Survived+10)
