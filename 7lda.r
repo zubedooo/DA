09ee69886198ca39928a1e0c23aa0fcbaff331aa
Zubin Jimmy Paymaster <zubinpaymaster@yahoo.co.in>
2019-12-02 23:15:41 +0530

    Update 7lda.r

--- 7lda.r
+++ 7lda.r
@@ -1,18 +1,51 @@
-df=read.csv('Advertising.csv')
-df_new=data.frame(df$TV,df$Sales)
-colnames(df_new)=c("TV","Sales")
-mean_tv=mean(df_new$TV)
-mean_sales=mean(df_new$Sales)
-m1=sum((df$TV-mean_tv)(df$Sales-mean_sales))/sum((df$TV-mean_tv)*2)
-m0=mean_sales-m1*mean_tv
-df_new$pred=m0+m1*df_new$TV
-plot(df$TV,df$Sales,col="red")
-lines(df$TV,df_new$pred,col="blue")
+library(MASS)
+library(ggplot2)
+titanicDS = read.csv("titanic.csv")
+dim(titanicDS)
 
-#Using prebuilt function
+str(titanicDS)
 
-lin_model=lm(Sales~TV,data=df_new)
-new=data.frame(TV=df$TV)
-pred=predict(lin_model,new)
-plot(df$TV,df$Sales)
-lines(df$TV,pred)
+summary(titanicDS)
+
+attach(titanicDS)
+UniqueValue = function (x) {length(unique(x)) }
+apply(titanicDS, 2, UniqueValue)
+
+NaValue = function (x) {sum(is.na(x)) }
+apply(titanicDS, 2, NaValue)
+
+BlankValue = function (x) {sum(x=="") }
+apply(titanicDS, 2, BlankValue)
+
+MissPercentage = function (x) {100 * sum (is.na(x)) / length (x) }
+apply(titanicDS, 2, MissPercentage)
+
+titanicDS$Age[is.na(titanicDS$Age)] = mean(titanicDS$Age, na.rm=TRUE)
+apply(titanicDS, 2, MissPercentage)
+
+set.seed(1)
+row.number = sample(1:nrow(titanicDS), 0.6*nrow(titanicDS))
+train = titanicDS[row.number,]
+test = titanicDS[-row.number,]
+dim(train)
+dim(test)
+
+# LDA model
+attach(train)
+lda.model = lda (factor(Survived)~factor(Pclass)+Sex+Age+SibSp, data=train)
+lda.model
+
+
+##Predicting training results.
+predmodel.train.lda = predict(lda.model, data=train)
+table(Predicted=predmodel.train.lda$class, Survived=Survived)
+
+ldahist(predmodel.train.lda$x[,1], g= predmodel.train.lda$class)
+
+
+attach(test)
+predmodel.test.lda = predict(lda.model, newdata=test)
+table(Predicted=predmodel.test.lda$class, Survived=test$Survived)
+
+par(mfrow=c(1,1))
+plot(predmodel.test.lda$x[,1], predmodel.test.lda$class, col=test$Survived+10)
